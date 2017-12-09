f17f0829237c9e08a50b27be52c4b2365fb6261b
Santosh Shetty <sshetty72@gmail.com>
2017-12-09 16:16:27 -0600

    Conditional statement to check for args.

--- src/main/java/com/mycompany/apps/pkg/App.java
+++ src/main/java/com/mycompany/apps/pkg/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	if (args.length != 0) {
+		System.out.println("Did not expect any parameters");
+		System.exit(-1);
+	}
+	System.exit(0);
     }
 }
